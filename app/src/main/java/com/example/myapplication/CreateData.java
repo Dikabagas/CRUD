177a4fd6466038d73a95ee6078f739238e6d43b9
Dikabagas <Andhikabagaskara909@gmail.com>
2020-05-14 00:23:28 +0700

    coba

---
+++ app/src/main/java/com/example/myapplication/CreateData.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CreateData extends AppCompatActivity {
+    protected Cursor cursor;
+    DataHelper dbHelper;
+    Button ton1, ton2;
+    EditText text1, text2, text3, text4, text5, text6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_data);
+
+        dbHelper = new DataHelper(this);
+        text1 = (EditText) findViewById(R.id.editText1);
+        text2 = (EditText) findViewById(R.id.editText2);
+        text3 = (EditText) findViewById(R.id.editText3);
+        text4 = (EditText) findViewById(R.id.editText4);
+        text5 = (EditText) findViewById(R.id.editText5);
+        text6 = (EditText) findViewById(R.id.editText6);
+        ton1 = (Button) findViewById(R.id.button1);
+        ton2 = (Button) findViewById(R.id.button2);
+
+        ton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // untuk menyimpan data
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("insert into mahasiswa(nisn, nama, tgl, jk, alamat, asal) values('" +
+                        text1.getText().toString() + "','" +
+                        text2.getText().toString() + "','" +
+                        text3.getText().toString() + "','" +
+                        text4.getText().toString() + "','" +
+                        text5.getText().toString() + "','" +
+                        text6.getText().toString() + "')");
+                Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_LONG).show();
+                MainActivity.ma.RefreshList();
+                finish();
+            }
+        });
+        ton2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+    }
+}
